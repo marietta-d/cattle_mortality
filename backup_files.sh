86c8179d984b123214b1b6a2381de3c8b44a5b20
Big Boy <bigboy.qub@gmail.com>
2024-08-10 11:21:28 +0100

    Backup 10/08/2024 11:21:28

---
+++ backup_files.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rsync -azvv ./models/ /media/BlueHDD/SurvivalAnalysis/models/
+rsync -azvv ./data/ /media/BlueHDD/SurvivalAnalysis/data/
+rsync -azvv ./images/ /media/BlueHDD/SurvivalAnalysis/images/
+
